15382663f93ed6f97c6e89498ce421054953f6d1
Michael Nutt <23107124+michaelnutt2@users.noreply.github.com>
2024-08-31 12:05:02 -0500

    Updating example file structure

---
+++ module[2]/AccountDemo_Python/account.py
@@ -0,0 +1,13 @@
+
+
+class Account:
+    """ Python implementation of 
+    def __init__(
+        self,
+        customerName: String = "Test Name",
+        accountBalance: float = 0f,
+        pin: String = "0000"
+    ):
+        self.customerName = customerName
+        self.accountBalance = accountBalance
+        self.pin = pin
